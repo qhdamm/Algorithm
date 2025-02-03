2faa6aab98497655521aa3e1059d89a191bdc15a
Bodam Kim <88372309+qhdamm@users.noreply.github.com>
2025-02-03 11:09:55 +0900

    update

--- 백준/Silver/10431. 줄세우기/줄세우기.py
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -5,13 +5,30 @@ num = int(input())
 
 for _ in range(num):
     count = 0
-    line = []
     test = list(map(int, input().split()))
     test_num, students = test[0], test[1:]
-    for student in students:
-        position = 0
-        while position < len(line) and line[position] < student:
-            position += 1
-        line.insert(position, student)
-        count += len(line) - position - 1
+    for i in range(20):
+        for j in range(i+1, 20):
+            if students[i] > students[j]:
+                students[i], students[j] = students[j], students[i]
+                count += 1
     print(test_num, count)
+
+# method2
+# import sys
+# from collections import Counter
+# input = sys.stdin.readline
+# num = int(input())
+
+# for _ in range(num):
+#     count = 0
+#     line = []
+#     test = list(map(int, input().split()))
+#     test_num, students = test[0], test[1:]
+#     for student in students:
+#         position = 0
+#         while position < len(line) and line[position] < student:
+#             position += 1
+#         line.insert(position, student)
+#         count += len(line) - position - 1
+#     print(test_num, count)
