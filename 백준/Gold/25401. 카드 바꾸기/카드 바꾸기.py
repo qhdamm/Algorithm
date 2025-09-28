ee1f33b25b3b6c50013647b421e7180d4637cd76
Bodam Kim <qhdamm23@naver.com>
2025-09-29 01:26:57 +0900

    [Gold V] Title: 카드 바꾸기, Time: 36 ms, Memory: 32412 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+card_list = list(map(int, input().split()))
+final_ans = float('inf')
+
+for i in range(N):
+    for j in range(i+1, N):
+        ans = 0
+        diff = (card_list[j] - card_list[i]) // (j-i)
+        for k in range(N):
+            exp_card = card_list[i] + diff*(k-i)
+            if exp_card != card_list[k]:
+                ans += 1
+        if ans <= final_ans:
+            final_ans = ans
+
+print(final_ans)        
