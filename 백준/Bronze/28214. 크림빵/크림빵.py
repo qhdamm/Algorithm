fd6e9c7dc3659267ec8f07e713f60ed2c2f349a4
Bodam Kim <qhdamm23@naver.com>
2025-09-29 14:21:52 +0900

    [Bronze III] Title: 크림빵, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -6,7 +6,7 @@ bread = list(map(int, input().split()))
 
 ans = 0
 for i in range(0, N*K, K):
-    bread_set = bread[i:(i+1)*K]
+    bread_set = bread[i:i+K]
     if bread_set.count(0) < P:
         ans += 1
 
