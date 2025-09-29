c9692f381d80cc5dbe5f437fd6b89aa9a9593886
Bodam Kim <qhdamm23@naver.com>
2025-09-29 16:28:05 +0900

    [Silver III] Title: 햄버거 분배, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -1,15 +1,18 @@
+# 사람을 기준으로 한 윈도우에서 가장 왼쪽의 햄버거 먹기
 import sys
 
-N, K = map(int, sys.stdin.readline().split())
-table = list(sys.stdin.readline().strip())
-
-answer = 0
+input = sys.stdin.readline
+N, K = map(int, input().split())
+ham_list = [ham for ham in input().strip()]
+ans = 0
 
 for i in range(N):
-    if table[i] == 'P':
-        for j in range(max(0, i-K), min(N, i+K+1)):
-            if table[j] == 'H':
-                table[j] = 'X'
-                answer += 1
+    if ham_list[i] == 'P':
+        eat_window = ham_list[max(0,i-K):min(i+K+1,N)]
+        for j in range(len(eat_window)):
+            if eat_window[j] == 'H':
+                ham_list[max(0,i-K)+j] = 'X'
+                ans += 1
                 break
-print(answer)
+
+print(ans)
