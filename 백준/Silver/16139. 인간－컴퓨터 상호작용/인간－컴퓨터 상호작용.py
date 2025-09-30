b20fb87f3476bb8a3f0ef049533e2034d5ac9590
Bodam Kim <qhdamm23@naver.com>
2025-10-01 01:52:10 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 320 ms, Memory: 161536 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,10 +1,26 @@
 import sys
 
 input = sys.stdin.readline
-text_list = [t for t in input()]
+text_list = [t for t in input().strip()]
 N = int(input())
+# (행)가로가 문자에서의 위치, (열)세로가 알파벳 -> 행 인덱스: 알파벳, 열 인덱스: 문자
+count_list = [[0]*len(text_list) for _ in range(26)]
+
+def get_index(text):
+    return ord(text) - ord('a')
+
+for i, t in enumerate(text_list):
+    text_index = get_index(t)
+    for j in range(26):
+        count_list[j][i] = count_list[j][i-1]
+    count_list[text_index][i] += 1
+
 
 for _ in range(N):
     target, start, end = input().split()
     start = int(start); end = int(end)
-    print(text_list[start:end+1].count(target))
+    idx = get_index(target)
+    if start == 0:
+        print(count_list[idx][end])
+    else:
+        print(count_list[idx][end] - count_list[idx][start-1]) 
