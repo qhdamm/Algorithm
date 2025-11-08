12e6846ce466b1d6990ea99733aee794670d9cd3
Bodam Kim <qhdamm23@naver.com>
2025-11-08 17:52:44 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -0,0 +1,10 @@
+n = int(input())
+dp = [1] * 10001
+for i in range(2, 10001):
+    dp[i] += dp[i-2]
+for i in range(3, 10001):
+    dp[i] += dp[i-3]
+
+for _ in range(n):
+    c = int(input())
+    print(dp[c])
