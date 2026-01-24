1f96b03507b7a0b98948f0d485268a4b31b2cc94
Bodam Kim <qhdamm23@naver.com>
2026-01-24 15:12:13 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,23 +1,14 @@
-nums = input()
+nums = input().strip()
 count_0 = 0
 count_1 = 0
-flag_0 = False
-flag_1 = False
+
+prev = None
 for n in nums:
-    if n == "1" and not flag_1:
-        count_1 += 1
-        flag_1 = True
-        flag_0 = False
-    elif n == "1" and flag_1:
-        flag_1 = True
-        flag_0 = False
-        continue
-    elif n == "0" and not flag_0:
-        count_0 += 1
-        flag_0 = True
-        flag_1 = False
-    elif n == "0" and flag_0:
-        flag_0 = True
-        flag_1 = False
-        continue
+    if n != prev:
+        if n == "0":
+            count_0 += 1
+        else:
+            count_1 += 1
+        prev = n
+
 print(min(count_0, count_1))
