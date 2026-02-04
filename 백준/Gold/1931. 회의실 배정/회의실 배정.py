272279e8e337cc419f1120d7c0ec1b31ea12b54d
Bodam Kim <qhdamm23@naver.com>
2026-02-04 15:18:40 +0900

    [Gold V] Title: 회의실 배정, Time: 256 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,18 +1,15 @@
-# 지금 시점에서 가장 빨리 끝나는 회의 선택
 import sys
 
 input = sys.stdin.readline
-N = int(input())
-room_list = []
-for _ in range(N):
-    room_list.append(list(map(int, input().split())))
+n = int(input())
+schedule = [list(map(int, input().split())) for _ in range(n)]
+schedule.sort(key=lambda x: (x[1], x[0]))
 
-# list의 각 원소 x에서 두번째, 첫번째 값을 뽑아서 정렬
-room_list.sort(key=lambda x: [x[1], x[0]])
-current_time = 0
-ans = 0
-for room in room_list:
-    if current_time <= room[0]:
+ans = 1
+start = schedule[0][1]
+for i in range(1, n):
+    if start <= schedule[i][0]:
         ans += 1
-        current_time = room[1]
+        start = schedule[i][1]
+
 print(ans)
