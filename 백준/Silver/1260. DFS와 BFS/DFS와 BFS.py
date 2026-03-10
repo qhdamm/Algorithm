97d195d6b6fe29f9daa462a4a0bc356bccbe3beb
Bodam Kim <qhdamm23@naver.com>
2026-03-10 13:01:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35572 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,48 @@
 import sys
 from collections import deque
-sys.setrecursionlimit(10**6)
+
+
 input = sys.stdin.readline
-n, m, v = map(int, input().split())
-graph = [[] for _ in range(n+1)]
-for _ in range(m):
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-for i in range(n+1):
-    graph[i] = sorted(graph[i])
+for i in range(N+1):
+    graph[i].sort()
 
-visited = [False] * (n+1)
+visited1 = [False] * (N+1)
 dfs_visit = []
-def dfs(x):
-    visited[x] = True
-    dfs_visit.append(x)
-    for n in graph[x]:
-        if visited[n]:
+def dfs(start):
+    stack = [start]
+    while stack:
+        x = stack.pop()
+        if visited1[x]:
             continue
-        dfs(n)
 
-visited2 = [False] * (n+1)
+        visited1[x] = True
+        dfs_visit.append(x)
+        for next_node in reversed(graph[x]):
+            if not visited1[next_node]:
+                stack.append(next_node)
+
+visited2 = [False] * (N+1)
 bfs_visit = []
-def bfs(x):
-    q = deque()
-    q.append(x)
-    visited2[x] = True
+def bfs(start):
+    q = deque([start])
+    visited2[start] = True
     while q:
-        nx = q.popleft()
-        bfs_visit.append(nx)
-        for n in graph[nx]:
-            if visited2[n]:
-                continue
-            q.append(n)
-            visited2[n] = True
+        x = q.popleft()
+        bfs_visit.append(x)
 
-dfs(v)
-bfs(v)
+        for next_node in graph[x]:
+            if not visited2[next_node]:
+                visited2[next_node] = True
+                q.append(next_node)
+
+dfs(V)
+bfs(V)
 print(*dfs_visit)
 print(*bfs_visit)
