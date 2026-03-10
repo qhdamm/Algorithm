920785644663316f14545a41350bc7e6391be68a
Bodam Kim <qhdamm23@naver.com>
2026-03-10 13:15:10 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,30 +1,29 @@
 import sys
 from collections import deque
+
+
 input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = [input().strip() for _ in range(n)]
 
-N, M = map(int, input().split())
-arr = []
-for _ in range(N):
-    arr.append(input().strip())
+visited = [[0]*m for _ in range(n)]
+dy = (1, -1, 0, 0)
+dx = (0, 0, 1, -1)
+ans = 0
+def bfs(sx, sy):
+    q = deque()
+    q.append((sy, sx))
+    visited[sy][sx] += 1
 
-visited = [[0]*M for _ in range(N)]
-def bfs():
-    queue = deque()
-    queue.append((0,0))
-    visited[0][0] += 1
-
-    dy = (0, 0, 1, -1)
-    dx = (1, -1, 0, 0)
-    while queue:
-        y, x = queue.popleft()
+    while q:
+        y, x = q.popleft()
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if 0<=ny<N and 0<=nx<M:
-                if visited[ny][nx] == 0 and arr[ny][nx] == "1":
+            if 0<=ny<n and 0<=nx<m:
+                if visited[ny][nx] == 0 and graph[ny][nx] == "1":
                     visited[ny][nx] = visited[y][x] + 1
-                    queue.append((ny, nx))
+                    q.append((ny, nx))
 
-bfs()
-print(visited[N-1][M-1])
-
+bfs(0, 0)
+print(visited[n-1][m-1])
