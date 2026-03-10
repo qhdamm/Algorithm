16588f0e6b16f636b2bfd4a9122671afe4cd4611
Bodam Kim <qhdamm23@naver.com>
2026-03-10 15:37:04 +0900

    [Gold V] Title: 토마토, Time: 2520 ms, Memory: 58468 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -13,34 +13,36 @@ for _ in range(h):
 dy = (0, 0, 1, -1, 0, 0)
 dx = (1, -1, 0, 0, 0, 0)
 dh = (0, 0, 0, 0, 1, -1)
-
-q = deque()
 visited = [[[-1]*m for _ in range(n)] for _ in range(h)]
-for k in range(h):
-    for j in range(n):
-        for i in range(m):
-            if tomato[k][j][i] == 1:
-                q.append((k, j, i))
-                visited[k][j][i] = 0
+q = deque()
 
-while q:
-    hi, yi, xi = q.popleft()
-    for d in range(6):
-        nh = hi + dh[d]
-        ny = yi + dy[d]
-        nx = xi + dx[d]
-        if 0<=nh<h and 0<=ny<n and 0<=nx<m:
-            if tomato[nh][ny][nx] == 0 and visited[nh][ny][nx] == -1:
-                visited[nh][ny][nx] = visited[hi][yi][xi] + 1
-                tomato[nh][ny][nx] = 1
-                q.append((nh, ny, nx))
+for z in range(h):
+    for y in range(n):
+        for x in range(m):
+            if tomato[z][y][x] == 1:
+                q.append((z, y, x))
+                visited[z][y][x] = 0
 
+
+def bfs():
+    while q:
+        ch, cy, cx = q.popleft()
+        for i in range(6):
+            nh = ch + dh[i]
+            ny = cy + dy[i]
+            nx = cx + dx[i]
+            if 0<=nx<m and 0<=ny<n and 0<=nh<h:
+                if tomato[nh][ny][nx] == 0 and visited[nh][ny][nx] == -1:
+                    q.append((nh, ny, nx))
+                    visited[nh][ny][nx] = visited[ch][cy][cx] + 1
+
+bfs() 
 ans = 0
-for k in range(h):
+for i in range(h):
     for j in range(n):
-        for i in range(m):
-            if tomato[k][j][i] == 0:
+        for k in range(m):
+            if tomato[i][j][k] == 0 and visited[i][j][k] == -1:
                 print(-1)
-                sys.exit(0)
-            ans = max(ans, visited[k][j][i])
+                exit()
+            ans = max(ans, visited[i][j][k])
 print(ans)
